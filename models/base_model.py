4d89ebf0411fe0d1d84254240aa2a420eb2b8fd6
mohamed-622 <mohamedezghoudi@gmail.com>
2023-08-12 09:17:07 -0700

    fix 'BaseModel' object has no attribute 'id'

--- models/base_model.py
+++ models/base_model.py
@@ -17,18 +17,21 @@ class BaseModel:
             - *args: list of arguments
             - **kwargs: dict of key-values arguments
         """
-        if kwargs:  # If kwargs is not empty
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.datetime.strptime(value,
-                                                                  '%Y-%m-%dT%H:%M:%S.%f'))
-                elif key != '__class__':
-                    setattr(self, key, value)
-                else:  # If kwargs is empty
-                    self.id = str(uuid.uuid4())
-                    self.created_at = datetime.datetime.now()
-                    self.updated_at = self.created_at
-                    models.storage.new(self)
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """
