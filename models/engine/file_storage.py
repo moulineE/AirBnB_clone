09e93cc927e401f717a830021a89a8393ca15a3c
Mohamed <mohamedezghoudi@gmail.com>
2023-08-13 17:24:04 +0100

    revert file_storage.py commit

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,10 +2,7 @@
 """Module for File_storage class
 Contains the file_storage class for the AirBnB clone console.
 """
-import cmd
 import json
-import re
-from shlex import split 
 from models.base_model import BaseModel
 from models.user import User
 from models.amenity import Amenity
@@ -13,24 +10,6 @@ from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
-
-
-def parse(arg):
-    curly_braces = re.search(r"\{(.*?)\}", arg)
-    brackets = re.search(r"\[(.*?)\]", arg)
-    if curly_braces is None:
-        if brackets is None:
-            return [i.strip(",") for i in split(arg)]
-        else:
-            lexer = split(arg[:brackets.span()[0]])
-            retl = [i.strip(",") for i in lexer]
-            retl.append(brackets.group())
-            return retl
-    else:
-        lexer = split(arg[:curly_braces.span()[0]])
-        retl = [i.strip(",") for i in lexer]
-        retl.append(curly_braces.group())
-        return retl
 
 
 class FileStorage:
