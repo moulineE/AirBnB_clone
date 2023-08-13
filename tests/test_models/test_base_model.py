059691abd7a2a2c586d8bc3423e9f44b20807deb
moulineE <elmahdi.mouline@live.fr>
2023-08-13 02:06:35 +0100

    test_base_model update

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""This module define a Python unittest for base_model.py
+
+Unittest classes:
+    base_model__Public_instance
+    base_model__save
+    base_model__to_dict
+
+"""
+
+
+import unittest
+import os
+import models
+from models.base_model import BaseModel
+
+
+class base_model__Public_instance(unittest.TestCase):
+    """unittestes for Task 3 & 4 testing the instantiation od BaseModel"""
+
+    def
